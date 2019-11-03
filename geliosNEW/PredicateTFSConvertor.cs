c2dfd44e406893ea62ebb458b3292e3d9e6808a9
atomaszz <finton2010@yandex.ru>
2019-11-03 18:49:46 +0300

    1

--- geliosNEW/PredicateTFSConvertor.cs
+++ geliosNEW/PredicateTFSConvertor.cs
@@ -6,33 +6,13 @@ using System.Threading.Tasks;
 
 namespace geliosNEW
 {
-    class TPredicateNumGenerator
-    {
-        int f_CurrentNum;
-        public TPredicateNumGenerator()
-        {
-            f_CurrentNum = 0;
-        }
-        public int NextNum()
-        {
-            f_CurrentNum++;
-            return f_CurrentNum;
-        }
-        public void InitNum(int ACurr = 0)
-        {
-            f_CurrentNum = ACurr;
-        }
-        public int NextLowNum()
-        {
-            f_CurrentNum--;
-            return f_CurrentNum;
-        }
-    }
     class TPredicateItemBase
     {
-        int f_ID;
+        private:
+    int f_ID;
         int f_NumAlt;
         bool f_Envelope;
+<<<<<<< HEAD
         TPredicateItemBig f_EnvelopeBIG;
         public TPredicateItemBase()
         {
@@ -642,6 +622,105 @@ namespace geliosNEW
     TPredicateItemBig f_PredicateStart;
     TPredicatePathItem f_BasePath;
     TPredicatePathItem f_UsedPath;
+=======
+        TPredicateItemBig* f_EnvelopeBIG;
+        public:
+    TPredicateItemBase();
+        virtual ~TPredicateItemBase() { return; }
+        virtual int Who() { return -1; }
+        virtual void ListIDFill(TDynamicArray* AList);
+        __property int NumAlt = { read = f_NumAlt, write = f_NumAlt };
+        __property bool Envelope = { read = f_Envelope, write = f_Envelope };
+        __property int ID = { read = f_ID, write = f_ID };
+        __property TPredicateItemBig* EnvelopeBIG = {read = f_EnvelopeBIG, write = f_EnvelopeBIG
+    };
+
+};
+
+class TPredicateItemTFE
+{
+    TTreeListItem f_TFE;
+    TPredicateItemBig* f_Big;
+    TAlternativeParserItemTFE* f_RfcTFE;
+    public:
+     TPredicateItemTFE();
+    ~TPredicateItemTFE();
+    __property TTreeListItem* TFE = {read = f_TFE, write = f_TFE
+};
+__property TPredicateItemBig* Big = { read = f_Big, write = f_Big };
+__property TAlternativeParserItemTFE* RfcTFE = { read = f_RfcTFE, write = f_RfcTFE };
+
+};
+
+
+class TPredicateItemTFS : public TPredicateItemBase
+{
+    private:
+      TTreeListTFS* f_TFS;
+TList* f_ListTFE;
+void FreeList();
+int __fastcall GetTFECount();
+TPredicateItemTFE* __fastcall GetTFEItems(int AIndex);
+public:
+      TPredicateItemTFS();
+~TPredicateItemTFS();
+int Who() { return 0; }
+void Assign(TAlternativeParserItemTFS* ATfs);
+void ListIDFill(TDynamicArray* AList);
+__property TTreeListTFS* TFS = { read = f_TFS };
+__property int TFECount = { read = GetTFECount };
+__property TPredicateItemTFE* TFEItems[int AIndex] = { read = GetTFEItems };
+
+};
+
+class TPredicateItemBig : public TPredicateItemBase
+{
+   private:
+      bool f_Print;
+TList* f_List;
+//      TPredicateItemTFE* f_OwnerTFE;
+TAlternativeParserItemBig* f_Rfc;
+int __fastcall GetCount();
+TPredicateItemBase* __fastcall GetItems(int AIndex);
+void FreeList();
+public:
+     int Who() { return 1; }
+TPredicateItemBig();
+~TPredicateItemBig();
+void AddItem(TPredicateItemBase* AItem);
+void DeleteItemToList(TPredicateItemBase* AItem);
+bool ValidDescendant();
+__property int Count = { read = GetCount };
+__property TPredicateItemBase* Items[int AIndex] = { read = GetItems };
+//     __property TPredicateItemTFE* OwnerTFE = {read = f_OwnerTFE, write = f_OwnerTFE};
+__property TAlternativeParserItemBig* Rfc = { read = f_Rfc, write = f_Rfc };
+__property bool Print = { read = f_Print, write = f_Print };
+};
+
+class TPredicateItemPWork : public TPredicateItemBase
+{
+    private:
+      TPredicateItemBase* f_Item1;
+TPredicateItemBase* f_Item2;
+public:
+      int Who() { return 2; }
+TPredicateItemPWork();
+~TPredicateItemPWork();
+void ListIDFill(TDynamicArray* AList);
+__property TPredicateItemBase* Item1 = { read = f_Item1, write = f_Item1 };
+__property TPredicateItemBase* Item2 = { read = f_Item2, write = f_Item2 };
+};
+
+
+class TPredicateTFSConvertor
+{
+    private:
+     TList* f_ListEnlarge;
+    TPredicateNumGenerator* f_NGen;
+    TPredicateItemBig* f_PredicateStart;
+    TPredicatePathItem* f_BasePath;
+    TPredicatePathItem* f_UsedPath;
+>>>>>>> parent of e98a560... ++
     int f_PathStyle;
     bool f_TryPath;
     void FreeHead();
